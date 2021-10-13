63463eec112935790ebf6e375db8f5ca264fe2fc
Helena Drielli <helenadrielli42@gmail.com>
2021-10-13 19:15:03 -0300

    Added an year filter to better track expenses from different years. Also added inputs to describe new expenses which is still in need of function.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseList from "./components/ExpenseList";
+import NewExpense from "./components/NewExpense/NewExpense";
+import ExpenseList from "./components/Expenses/ExpenseList";
 
 function App() {
   const expenses = [
@@ -28,9 +29,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js')
+    console.log(expense)
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <ExpenseList expenses={expenses} />
     </div>
   );
